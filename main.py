f3ce2ec5edbbb623a3e7091bb3f1f3430a36d251
deckvrd <deckvrd@gmail.com>
2020-11-30 17:04:40 -0500

    docs: add comments
    Update code with comments explaining code.

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
+#takes file as argument and stores in an array
 def readContents(file):
     content = []
     with open(file) as f:
         for line in f:
+            #strip newlines and appends to content array
             content.append(line.rstrip("\n"))
         return content
 
+#takes filename followed by two files as arguments
 def createTags(fileName, numbers, points):
     for x in numbers:
         for y in points:
             processTags(fileName, x, y)
 
+#writes tags to new file
 def processTags(fileName, x, y):
     f = open("build/" + fileName + ".txt", "a")
     tags = y.replace("01", x)
